5682715bf48f860cacb6fa5268680ad4d5d61a0e
Kian Walker <221608370+kianish675@users.noreply.github.com>
2026-02-18 10:16:27 +0000

    prebuilts: r9qxxx: Nuke Legacy VNDK
    The A73 firmware comes with it. Why add it from another device
    
    Signed-off-by: Kian Walker <221608370+kianish675@users.noreply.github.com>

--- unica/patches/legacy_stack/customize.sh
+++ unica/patches/legacy_stack/customize.sh
@@ -35,9 +35,7 @@ echo "system/system_ext/lib 0 0 755 capabilities=0x0" >> "$WORK_DIR/configs/fs_c
 echo "/system/system_ext/lib u:object_r:system_lib_file:s0" >> "$WORK_DIR/configs/file_context-system"
 
 echo "Adding Legacy WFD"
-echo "Adding Legacy VNDK"
 ADD_TO_WORK_DIR "r9qxxx" "system" "bin" 0 2000 751 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "r9qxxx" "system" "lib" 0 0 755 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "r9qxxx" "system" "system_ext/apex/com.android.vndk.v30.apex" 0 0 755 "u:object_r:system_file:s0"
 
 echo "Legacy stack was applied successfully!"
