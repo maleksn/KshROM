9c289879a34c0637907b762226b2450e0a992fc4
Kian Walker <221608370+kianish675@users.noreply.github.com>
2026-02-25 04:41:51 +0000

    target: a70q: debloat.sh: use "$TARGET_CODNEAME" for debloating the product rro now
    Signed-off-by: Kian Walker <221608370+kianish675@users.noreply.github.com>

--- target/a70q/debloat.sh
+++ target/a70q/debloat.sh
@@ -27,7 +27,7 @@ system/app/WifiRROverlayAppH2E
 
 # Device Specific Overlay
 PRODUCT_DEBLOAT+="
-overlay/framework-res__a73xqxx__auto_generated_rro_product.apk
+overlay/framework-res__${SOURCE_CODENAME}__auto_generated_rro_product.apk
 "
 
 # Google Assistant
